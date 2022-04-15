1be2a2103825c120332d4ea933bd94210936f15d
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-04-14 21:26:57 -0300

    Express w/ NodeJs

---
+++ NodeWithTest/express1.js
@@ -0,0 +1,8 @@
+// Express é um framework minimalista
+// Express é um módulo, pode ser carregado dentro dos projetos. 
+/*
+Para instalar o Express:
+at windows CMD:
+$ npm install express --save
+*/
+
