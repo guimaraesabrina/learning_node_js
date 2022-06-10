aa2aca90bc802e330750dc0ea1cf1bbc72203bf7
Sabrina S. Guimarães <guimaraesadev@gmail.com>
2022-06-10 11:08:23 -0300

    tratando erros com throw new Error e adicionando fuc. async

--- projeto-markdown/javascript/index.js
+++ projeto-markdown/javascript/index.js
@@ -1,50 +1,18 @@
-// to deal with promises
-// .then() or
-// async and await 
 
 const fs = require('fs');
-const { get } = require('http');
-//filesystem
-// native javascript library
 
-//handling errors
-
-function handleErrors(erro) {
-    throw new Error(erro.code,'the file does not exist!'); //erro.code = passing the error code
+function trataErro(erro) {
+  throw new Error((erro.code, 'não há arquivo no caminho'));
 }
 
-function getFile(pathFile) {
-    const encoding = 'utf-8'
-    fs.promises
-        .readFile(pathFile, encoding)
-        .then((text) => console.log(text))
-        .catch((erro) => trataErro(erro))
-
+async function pegaArquivo(caminhoDoArquivo) {
+  const encoding = 'utf-8';
+  try {
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(texto)
+  } catch(erro) {
+    trataErro(erro);
+  }
 }
 
-getFile('../assets/index.md')
-
-//running fylesystem 
-// synchronous code vs asynchronous code
-// asynchronous code: sent the request and
-// and while the answer doesn't come, it will do something else
-
-// sync: telephone
-// async: whatspp/telegram
-
-// javascript calls asynchronous code "promises" = without stopping code execution
-
-
-// first method; 
-
-/*
-function getFile(pathFile) {
-    const encoding = 'utf-8'
-    fs.readFile(pathFile, encoding, (erro, text) => {
-        if (erro) {
-            handleErrors(erro)
-        }
-        console.log(text)
-    } )
-}
-*/
+pegaArquivo('../assets/index.md');
---
+++ projeto-markdown/javascript/pre-code.js
@@ -0,0 +1,52 @@
+// código para testes e rascunho
+
+// to deal with promises
+// .then() or
+// async and await 
+
+const fs = require('fs');
+const { get } = require('http');
+//filesystem
+// native javascript library
+
+//handling errors
+
+function handleErrors(erro) {
+    throw new Error(erro.code,'the file does not exist!'); //erro.code = passing the error code
+}
+
+function getFile(pathFile) {
+    const encoding = 'utf-8'
+    fs.promises
+        .readFile(pathFile, encoding)
+        .then((text) => console.log(text))
+        .catch((erro) => trataErro(erro))
+
+}
+
+getFile('../assets/index.md')
+
+//running fylesystem 
+// synchronous code vs asynchronous code
+// asynchronous code: sent the request and
+// and while the answer doesn't come, it will do something else
+
+// sync: telephone
+// async: whatspp/telegram
+
+// javascript calls asynchronous code "promises" = without stopping code execution
+
+
+// first method; 
+
+/*
+function getFile(pathFile) {
+    const encoding = 'utf-8'
+    fs.readFile(pathFile, encoding, (erro, text) => {
+        if (erro) {
+            handleErrors(erro)
+        }
+        console.log(text)
+    } )
+}
+*/
